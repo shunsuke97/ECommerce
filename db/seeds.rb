7d660fe8d930bed955df9c91b48c1162acd97c72
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-09-14 22:55:21 +0900

    update_seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 # create!は、作成に失敗したときに例外を発生させて処理を止める
 # seedsは必ず成功してデータを保存してほしいため、create!を使用する
-Product.create!(
-  name: Faker::Book.title,
-  description: Faker::Lorem.paragraph(2),
-  price: Faker::Number.within(100..100000),
-  # %w(yen usd) == ["yen", "usd"]
-  unit: %w(yen usd).sample
-)
+20.times do
+  Product.create!(
+    name: Faker::Book.title,
+    description: Faker::Lorem.paragraph(2),
+    price: Faker::Number.within(100..100000),
+    # %w(yen usd) == ["yen", "usd"]
+    unit: %w(yen usd).sample
+  )
+end
