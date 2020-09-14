0131bc5d4825f01455ef7f9bf5a7aa4b0ad59a75
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-09-14 22:47:03 +0900

    add_product_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create!は、作成に失敗したときに例外を発生させて処理を止める
+# seedsは必ず成功してデータを保存してほしいため、create!を使用する
+Product.create!(
+  name: Faker::Book.title,
+  description: Faker::Lorem.paragraph(2),
+  price: Faker::Number.within(100..100000),
+  # %w(yen usd) == ["yen", "usd"]
+  unit: %w(yen usd).sample
+)
