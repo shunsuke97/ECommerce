10723315224b1be878b169cc13ca32f1cacfa381
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-09-16 02:01:18 +0900

    add_extension_whitelist
    拡張子の制限を追加。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,2 +1,12 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class ImageUploader < ApplicationUploader
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  def accept_extensions
+    # map を使って "image/jpg" "image/jpeg" ... という要素を持つ配列を作成
+    accept_list = extension_whitelist.map { |e| "image/#{e}" }
+    #  "," でつなげた文字列を取得
+    accept_list.join(",")
+  end
 end
