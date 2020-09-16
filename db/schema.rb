694598481a211be7957a85d263c335c54ec77ea3
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-09-16 20:30:31 +0900

    create_basket.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_151835) do
+ActiveRecord::Schema.define(version: 2020_09_16_091954) do
+
+  create_table "baskets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_baskets_on_user_id"
+  end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_09_15_151835) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "baskets", "users"
 end
