74634d4c0a2d0d23f2f2da7ed7e7c54b06ebc3e0
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-09-17 00:17:15 +0900

    Update add_to_baskets_controller.rb
    商品を買い物カゴに追加したときに、買い物カゴにリダイレクトされるように変更。

--- app/controllers/products/add_to_baskets_controller.rb
+++ app/controllers/products/add_to_baskets_controller.rb
@@ -4,5 +4,7 @@ class Products::AddToBasketsController < Products::ApplicationController
     product = Product.find(params[:product_id])
     # 商品を選択する際に、ユーザの入力ミスはありえない為、create!を使用する
     basket.basket_products.create!(product_id: product.id)
+
+    redirect_to basket_path
   end
 end
