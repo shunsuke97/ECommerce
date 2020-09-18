98f0ff749319a1879fff339780fb55585320a668
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-09-18 17:25:50 +0900

    refactoring_basket.rb

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,18 +4,9 @@ class ChargesController < ApplicationController
     token = params[:stripeToken]
     product_ids = params[:product_ids].map(&:to_i)
 
-    products = current_user.basket.products.where(id: product_ids)
-    total_jpy = products.where(unit: :yen).sum(:price)
-    total_usd = products.where(unit: :usd).sum(:price)
-    # Basket classにある定義されているUSD_RATEを使用
-    total = total_jpy + total_usd * Basket::USD_RATE
+    total = current_user.basket.total_price(product_ids: product_ids)
 
-    Stripe::Charge.create({
-      amount: total,
-      currency: 'jpy',
-      description: 'Example charge',
-      source: token
-    })
+    Charge.create!(total, token)
     redirect_to root_path, notice: '決済に成功しました'
   end
 end
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -5,18 +5,26 @@ class Basket < ApplicationRecord
   # 変換レートは固定にするため、定数にて定義
   USD_RATE = 110.freeze
 
-  def total_price
-    total_jpy_price + total_usd_price
+  # キーワード引数にデフォルト値を設定することで、引数を渡さなくてもエラーが起こらなくなる。
+  def total_price(products_ids: nil)
+    total_jpy_price(product_ids) + total_usd_price(product_ids)
   end
 
   private
-  def total_jpy_price
+  def total_jpy_price(product_ids)
+    # if product_ids
+    #   products = self.products.where(id: product_ids)
+    # else 
+    #   products = self.products
+    # end
+    products = product_ids ? self.products.where(id: product_ids) : self.products
     jp_products = products.where(unit: 'yen')
     # sumを使用してpriceの合計値を計算
     jp_products.sum(:price)
   end
 
-  def total_usd_price
+  def total_usd_price(product_ids)
+    products = product_ids ? self.products.where(id: product_ids) : self.products
     us_products = products.where(unit: 'usd')
     us_products.sum(:price) * USD_RATE
   end
