556809bbc9a96cf3a15cc073611dc0eaa676464b
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-09-18 17:32:18 +0900

    Delete purchased items from the shopping cart

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,6 +6,10 @@ class ChargesController < ApplicationController
 
     total = current_user.basket.total_price(product_ids: product_ids)
 
+    # 買い物かごから商品を削除
+    basket_products = current_user.basket.basket_products.where(product_id: product_ids)
+    basket_products.each(&:destroy!)
+
     Charge.create!(total, token)
     redirect_to root_path, notice: '決済に成功しました'
   end
