78be97f78c68a5f863ed81ecdb2a75cd541f9b98
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-09-24 21:19:29 +0900

    Update application_controller.rb
    サインイン後に、Userのインスタンスであればroot_pathへ、Adminのインスタンスであればadmins_root_pathへリダイレクトさせる。
    Deviseの「after_sign_in_path_for」メソッドを使用。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # サインイン後に、Userのインスタンスであればroot_pathへ、Adminのインスタンスであればadmins_root_pathへリダイレクトさせる
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      root_path
+    elsif resource.is_a?(Admin)
+      admins_root_path
+    end
+  end
 end
